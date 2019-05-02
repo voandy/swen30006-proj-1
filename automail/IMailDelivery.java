8ad2070ac50d6ab44b64884ab91c59fbc44d2f85
Lachlan Grant <grantl1@student.unimelb.edu.au>
2019-05-02 15:33:42 +1000

    Update IMailDelivery.java

--- automail/IMailDelivery.java
+++ automail/IMailDelivery.java
@@ -1,14 +1,37 @@
-package automail;
+package strategies;
+
+import automail.MailItem;
+import automail.Robot;
+import automail.Team;
+import exceptions.ExcessiveDeliveryException;
+import exceptions.ItemTooHeavyException;
 
 /**
- * a MailDelivery is used by the Robot to deliver mail once it has arrived at the correct location
+ * addToPool is called when there are mail items newly arrived at the building to add to the MailPool or
+ * if a robot returns with some undelivered items - these are added back to the MailPool.
+ * The data structure and algorithms used in the MailPool is your choice.
+ * 
  */
-public interface IMailDelivery {
+public interface IMailPool {
+	
+	/**
+     * Adds an item to the mail pool
+     * @param mailItem the mail item being added.
+     */
+    void addToPool(MailItem mailItem);
+    
+    /**
+     * load up any waiting robots with mailItems, if any.
+     */
+	void step() throws ItemTooHeavyException, ExcessiveDeliveryException;
 
 	/**
-     * Delivers an item at its floor
-     * @param mailItem the mail item being delivered.
-     */
-	void deliver(MailItem mailItem);
-    
+     * @param robot refers to a robot which has arrived back ready for more mailItems to deliver
+     */	
+	void registerWaiting(Robot robot);
+	
+	public void disbandTeam(Team team);
+	
+	public void addRobotToTeam(Team team);
+
 }
