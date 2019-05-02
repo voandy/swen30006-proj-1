3545958972789b0e279affe91bffad97e7dc1bc5
Lachlan Grant <grantl1@student.unimelb.edu.au>
2019-05-02 15:33:09 +1000

    Update Team.java

--- automail/Team.java
+++ automail/Team.java
@@ -2,50 +2,99 @@ package automail;
 
 import java.util.ArrayList;
 
+import exceptions.ExcessiveDeliveryException;
+import strategies.IMailPool;
+import strategies.MailPool;
 
-public class Team {
-	private String id;
+
+public class Team implements Deliverer {
+	public static final int teamStepRate = 3;
+	
 	private ArrayList<Robot> robotList;
 	private MailItem heavyDeliveryItem;
-	private int stepsTaken;
+	private int stepCounter;
+	private int expectedNumberOfRobots;
 	
 	/**
 	 * 
-	 * @param teamRobotList	-	the provided robots
 	 * @param item - 			the mail item to be delivered
 	 */
-	/*public Team(ArrayList<Robot> teamRobotList, MailItem item) {
-		if(item.getWeight() > 2600 && item.getWeight() <= 3000) {
-			// 3 robot team
-			assert(teamRobotList.size() == 3);
-		} else {
-			// 2 robot team
-			assert(teamRobotList.size() == 2);
-		}
-		
-		// copy across
-		robotList = (ArrayList<Robot>)teamRobotList.clone();
-	}*/
-	
 	public Team(MailItem item) {
 		this.heavyDeliveryItem = item;
+		if(item.getWeight() > Robot.INDIVIDUAL_MAX_WEIGHT && item.getWeight() <= 2600) {
+			expectedNumberOfRobots = 2;
+		} else {
+			expectedNumberOfRobots = 3;
+		}
 		this.robotList = new ArrayList<Robot>();
+	}
+	
+	public Team(Team team) {
+		this.robotList = team.robotList;
+		this.heavyDeliveryItem = team.heavyDeliveryItem;
+		this.stepCounter = team.stepCounter;
+		this.expectedNumberOfRobots = team.expectedNumberOfRobots;
 	}
 
 	public ArrayList<Robot> getRobotList() {
 		return robotList;
 	}
 	
-	private void step() {
-		// TODO
-		;
+	public void step() throws ExcessiveDeliveryException {
+		if(stepCounter == teamStepRate) {
+			stepCounter = 0;
+			// do something
+			// call step on the leader robot
+		} else {
+			this.stepCounter++;
+		}
+		
+		for(Robot r: robotList) {
+			r.step();
+		}
 	}
 	
 	public MailItem getMailItem() {
 		return this.heavyDeliveryItem;
 	}
 	
+	
+	public void disbandTeam() {
+		IMailPool mailPool = this.robotList.get(0).getMailPool();
+		mailPool.disbandTeam(this);
+	}
 	// once delivery made
 	/* team.getRobotList().get(0).mailPool.disbandTeam() */
 	/* each robot gets put back to DELIVERING_AS_SINGLE state */
+	
+	public void dispatch() {
+		for(Robot r: robotList) {
+			r.resetDeliveryCounter();
+			r.setRouteInTeam(heavyDeliveryItem.getDestFloor());
+			r.setState(Robot.RobotState.DELIVERING_AS_SINGLE);
+		}
+	}
+	
+	public void addRobot(Robot robot) {
+		robotList.add(robot);
+		robot.setInTeam(true);
+	}
+	
+	public boolean robotListIsFull() {
+		if(robotList.size() == expectedNumberOfRobots) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean tubesFilled() {
+		for(Robot r: robotList) {
+			// assume that if getTube() is null then there
+			// is nothing in the tube
+			if(r.getTube() == null) {
+				return false;
+			}
+		}
+		return false;
+	}
 }
